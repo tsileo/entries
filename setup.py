7f4be92d2db7225e6aff288891fa78d3e2847c76
Thomas Sileo <t@a4.io>
2019-06-20 00:05:36 +0200

    Add basic gitignore

--- setup.py
+++ setup.py
@@ -3,7 +3,13 @@ from setuptools import setup
 setup(
     name="entries",
     version="0.1.1",
+    url="github.com/tsileo/entries",
+    description="Micropub client for the terminal",
+    author="Thomas Sileo",
+    author_email="t@a4.io",
+    license="OSI Approved :: ISC License (ISCL)",
     py_modules=["cli"],
     install_requires=["pyyaml", "mf2py", "click", "requests"],
     entry_points={"console_scripts": ["entries = cli:cli"]},
+    python_requires=">=3.6",
 )
